f69e4107a854c60a55559782c303d16bff913356
saulordz@gmail.com <saulordz@gmail.com>
2016-04-16 22:33:12 -0400

    Add onItemClickListener

--- app/src/main/java/saulo/com/sunshine/ForecastFragment.java
+++ app/src/main/java/saulo/com/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package saulo.com.sunshine;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -72,6 +75,19 @@ public class ForecastFragment extends Fragment {
         mListView = (ListView) rootView.findViewById(R.id.f_main_list_view);
         mListView.setAdapter(mAdapter);
 
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Context context = view.getContext();
+                CharSequence text = mForecastData.get(position);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+        });
         return rootView;
     }
 
