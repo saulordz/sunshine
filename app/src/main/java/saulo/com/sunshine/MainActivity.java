adff43c52997dd724a9cde13fa76eb764caf6186
saulordz@gmail.com <saulordz@gmail.com>
2016-04-17 00:16:47 -0400

    Add menu item to show map

--- app/src/main/java/saulo/com/sunshine/MainActivity.java
+++ app/src/main/java/saulo/com/sunshine/MainActivity.java
@@ -1,25 +1,12 @@
 package saulo.com.sunshine;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,7 +43,27 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_show_map) {
+            showMap();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showMap() {
+        String location = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.pref_location_text), getString(R.string.default_location_value));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
