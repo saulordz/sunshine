f8fe7f740fb63517040dca735e86549d31dc1b61
saulordz@gmail.com <saulordz@gmail.com>
2016-04-21 10:04:22 -0400

    Add style to the action bar

--- app/src/main/java/saulo/com/sunshine/MainActivity.java
+++ app/src/main/java/saulo/com/sunshine/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
