cf9375d7860e7cd80a6aba86664ebaf0c7ea19bf
saulordz@gmail.com <saulordz@gmail.com>
2016-05-10 10:30:03 -0400

    Fix tablet layouts

--- app/src/main/java/saulo/com/sunshine/DetailFragment.java
+++ app/src/main/java/saulo/com/sunshine/DetailFragment.java
@@ -22,7 +22,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.util.Util;
 
 import saulo.com.sunshine.data.WeatherContract;
 
