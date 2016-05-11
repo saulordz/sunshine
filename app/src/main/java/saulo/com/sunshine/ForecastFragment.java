602f2ad4cc92f0f0549e306640ee4a0c35a31007
saulordz@gmail.com <saulordz@gmail.com>
2016-05-11 01:56:20 -0400

    Remove not used code

--- app/src/main/java/saulo/com/sunshine/ForecastFragment.java
+++ app/src/main/java/saulo/com/sunshine/ForecastFragment.java
@@ -79,10 +79,6 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         void onItemSelected(Uri uri, ForecastAdapter.ForecastAdapterViewHolder vh);
     }
 
-    public void notifyDataChanged(){
-        mAdapter.notifyDataSetChanged();
-    }
-
     public ForecastFragment() {
     }
 
