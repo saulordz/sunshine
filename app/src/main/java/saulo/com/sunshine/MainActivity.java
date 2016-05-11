952e1cced69cec4fbb551b9f081c14e913ae150b
saulordz@gmail.com <saulordz@gmail.com>
2016-05-10 22:52:35 -0400

    Add current location

--- app/src/main/java/saulo/com/sunshine/MainActivity.java
+++ app/src/main/java/saulo/com/sunshine/MainActivity.java
@@ -1,10 +1,18 @@
 package saulo.com.sunshine;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.FragmentTransaction;
@@ -19,24 +27,33 @@ import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.pushbots.push.Pushbots;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import saulo.com.sunshine.gcm.RegistrationIntentService;
 import saulo.com.sunshine.sync.SunshineSyncAdapter;
 
-public class MainActivity extends AppCompatActivity implements ForecastFragment.CallbackForecastFragment {
+public class MainActivity extends AppCompatActivity implements ForecastFragment.CallbackForecastFragment, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
     private static final String TAG = "MainActivityTAG_";
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 101;
+
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String PROJECT_NUMBER = "165515085157";
 
     private static int selectionCount = 0;
 
-    private ShareActionProvider mShareActionProvider;
     private String mLocation;
     private boolean mTwoPane;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +63,15 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         Toolbar toolbar = (Toolbar) findViewById(R.id.a_main_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
+
+
+        //        if (checkForPermissions()) {
+//            connectToLocationAPI();
+//        } else {
+//            askForPermissions();
+//        }
+
+//        saveLocationPreference(30339+"");
 
         mLocation = Utility.getPreferredLocation(getApplicationContext());
 
@@ -78,7 +104,29 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         }
 
         Pushbots.sharedInstance().init(this);
-//        Pushbots.sharedInstance().setCustomHandler(PushBotsReceiver.class);
+
+    }
+
+    private void askForPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_LOCATION);
+    }
+
+    private void connectToLocationAPI() {
+        mGoogleApiClient = buildGoogleApiClient();
+        mGoogleApiClient.connect();
+    }
+
+    private boolean checkForPermissions() {
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean checkPlayServices() {
@@ -177,5 +225,74 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
                             new Pair<View, String>(vh.iconView, getString(R.string.detail_icon_transition_name)));
             ActivityCompat.startActivity(this, intent, activityOptions.toBundle());
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+        //supress the warning since we already checked for the permission
+        @SuppressWarnings("MissingPermission") Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        String zipCode = null;
+        try {
+            zipCode = geocoder.getFromLocation(latitude,longitude,1).get(0).getPostalCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(zipCode!=null){
+            saveLocationPreference(zipCode);
+        }
+        Log.d(TAG, "onConnected: " + location.toString());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    connectToLocationAPI();
+
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended: ");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: ");
+    }
+
+    protected synchronized GoogleApiClient buildGoogleApiClient() {
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    public void saveLocationPreference(String location){
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(getString(R.string.pref_location_key), location);
+        editor.commit();
+
+        SunshineSyncAdapter.syncImmediately(this);
     }
 }
